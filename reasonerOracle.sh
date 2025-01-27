0821f7abcf6360ac5aa3ebea5065430d4740ba6c
Tobias John <tobiajoh@ifi.uio.no>
2025-01-27 11:08:56 +0100

    oracle with timeout

--- reasonerOracle.sh
+++ reasonerOracle.sh
@@ -3,21 +3,31 @@
 # runs test on provided .owl file
 # returns "pass" or "fail", depending on whether the reasoners agree with each other
 
+# optinal argument: timeout (in seconds)
+
+
 ontFile=$1
+maxTime=${2:-86400} # no timeout: 24h
 
 
 # log file 
 log="temp_reasoners.log"
 rm -f $log
 
+wasTimeout=0
+
 # use java properties file to prevent unnormal logging
-java -Djava.util.logging.config.file=api_tester/src/main/resources/logging.properties -jar api_tester/target/api_tester-1.0-SNAPSHOT.jar $ontFile nothing nothing --test-reasoners --no-export >> $log 2>&1
+timeout $maxTime java -Djava.util.logging.config.file=api_tester/src/main/resources/logging.properties -jar api_tester/target/api_tester-1.0-SNAPSHOT.jar $ontFile nothing nothing --test-reasoners --no-export >> $log 2>&1
 #java -jar api_tester/target/api_tester-1.0-SNAPSHOT.jar $ontFile nothing nothing --test-reasoners --no-export >> $log 2>&1
 
-if [[ $? == 1 ]] ; then
-    # create output, if error is found
+exitCode=$?
+
+if [[ $exitCode == 124 ]] ; then
+    # time limit exceeded
+    echo "timeout"
+elif [[ $exitCode == 1 ]] ; then
     echo "fail"
-    count=$((count+1))
 else
 	echo "pass"
 fi
+
